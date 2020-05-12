bfdce5ea089f7a71bb31fee892ded24ddedda2ba
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-12 17:40:44 +0200

    Added and used Integration Fixture

---
+++ ConnectingApps.IntegrationFixture/Fixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WireMock.Server;
+
+namespace ConnectingApps.IntegrationFixture
+{
+    public class Fixture<TStartup> where TStartup : class
+    {
+        private readonly Dictionary<string, string> _configurationDictionary = new Dictionary<string, string>();
+        private readonly IntegrationWebApplicationFactory<TStartup> _factory = new IntegrationWebApplicationFactory<TStartup>();
+        private readonly Lazy<IServiceScope> _serviceScope;
+
+        public Fixture()
+        {
+            _serviceScope = new Lazy<IServiceScope>(CreateServiceScope);
+        }
+
+        private IServiceScope CreateServiceScope()
+        {
+            IServiceScope serviceScope = null;
+            _factory.WithWebHostBuilder(whb =>
+            {
+                whb.ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.AddInMemoryCollection(_configurationDictionary);
+                });
+                whb.ConfigureTestServices(sc =>
+                {
+                    serviceScope = sc.BuildServiceProvider().CreateScope();
+                });
+            }).CreateClient().Dispose();
+            return serviceScope;
+        }
+
+        public FluentMockServer FreezeServer(string configurationParameter)
+        {
+            var server = FluentMockServer.Start();
+            var url = server.Urls.Single();
+            _configurationDictionary.Add(configurationParameter, url);
+            return server;
+        }
+
+        public TTestType Create<TTestType>() where TTestType : class
+        {
+            return _serviceScope.Value.ServiceProvider.GetService<TTestType>();
+        }
+    }
+}
---
+++ ConnectingApps.IntegrationFixtureTests/SearchEngineControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectingApps.DncWireMockDemo;
+using ConnectingApps.DncWireMockDemo.Controllers;
+using ConnectingApps.IntegrationFixture;
+using Microsoft.AspNetCore.Mvc;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using Xunit;
+
+namespace ConnectingApps.IntegrationFixtureTests
+{
+    public class SearchEngineControllerTests
+    {
+
+        [Fact]
+        public async Task GetTest()
+        {
+            var fixture = new Fixture<Startup>();
+
+            using (var mockServer = fixture.FreezeServer("Google"))
+            {
+                SetupStableServer(mockServer, "Response");
+                var controller = fixture.Create<SearchEngineController>();
+                var response = await controller.GetNumberOfCharacters("Hoi");
+
+                var request = mockServer.LogEntries.Select(a => a.RequestMessage).Single();
+                Assert.Contains("Hoi", request.RawQuery);
+                Assert.Equal(8, ((OkObjectResult)response.Result).Value);
+            }
+        }
+
+        private void SetupStableServer(FluentMockServer fluentMockServer, string response)
+        {
+            fluentMockServer.Given(Request.Create().UsingGet())
+                .RespondWith(Response.Create().WithBody(response, encoding: Encoding.UTF8)
+                    .WithStatusCode(HttpStatusCode.OK));
+        }
+
+    }
+}
