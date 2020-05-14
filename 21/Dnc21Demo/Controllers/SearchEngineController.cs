44291029591fe0d076024c91b0de95b61dfaa172
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-14 14:30:05 +0200

    Added a .NET Core 2.1  web api project

---
+++ 21/Dnc21Demo/Controllers/SearchEngineController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using ConnectingApps.DncWireMockDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dnc21Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchEngineController : ControllerBase
+    {
+        private readonly ISearchEngineService _searchEngineService;
+
+        public SearchEngineController(ISearchEngineService searchEngineService)
+        {
+            _searchEngineService = searchEngineService;
+        }
+
+        [HttpGet("{queryEntry}", Name = "GetNumberOfCharacters")] // http://localhost:61712/api/searchengine/daan
+        public async Task<ActionResult<int>> GetNumberOfCharacters(string queryEntry)
+        {
+            var numberOfCharacters = await _searchEngineService.GetNumberOfCharactersFromSearchQuery(queryEntry);
+            return Ok(numberOfCharacters);
+        }
+    }
+}
---
+++ 21/Dnc21Demo/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using ConnectingApps.DncWireMockDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+
+namespace Dnc21Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var googleLocation = Configuration["Google"];
+            services.AddHttpClient<ISearchEngineService, SearchEngineService>(c =>
+                    c.BaseAddress = new Uri(googleLocation))
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .AddPolicyHandler(GetRetryPolicy());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError().OrTransientHttpStatusCode()
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,
+                    retryAttempt)));
+        }
+    }
+}
