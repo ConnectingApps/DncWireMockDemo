b7ceb14ac27a54f7b165b35b0e3ecc52b88209a1
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-23 18:13:45 +0200

    Added tests for .net core 2.1

---
+++ 21/Dnc21Demo/Controllers/LogicController.cs
@@ -0,0 +1,34 @@
+﻿using ConnectingApps.DncWireMockDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Dnc21Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogicController : ControllerBase
+    {
+        private readonly ILogger<LogicController> _logger;
+
+        public LogicController(ILogger<LogicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] Name value)
+        {
+            return Ok($"{value.FirstName} {value.MiddleName} {value.LastName}");
+        }
+
+        [HttpPut]
+        public ActionResult<string> Put([FromBody] Name value)
+        {
+            _logger.LogWarning("Warning Logged");
+
+            _logger.LogInformation("This is the input input {name}", value);
+
+            return Ok($"{value.FirstName} {value.MiddleName} {value.LastName}");
+        }
+    }
+}
