e3af5199499739a7763f0150213e9e05793fcbb9
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-26 17:22:48 +0200

    Tested exceptions of the Freeze method

---
+++ ConnectingApps.IntegrationFixtureTests/FreezeTests.cs
@@ -0,0 +1,51 @@
+﻿using ConnectingApps.DncWireMockDemo;
+using ConnectingApps.DncWireMockDemo.Services;
+using ConnectingApps.IntegrationFixture;
+using ConnectingApps.IntegrationFixture.Shared;
+using Moq;
+using Refit;
+using Xunit;
+
+namespace ConnectingApps.IntegrationFixtureTests
+{
+    public class FreezeTests
+    {
+        [Fact]
+        public void DoubleCallRefitFixture()
+        {
+            using (var fixture = new RefitFixture<Startup, ISearchEngine>(RestService.For<ISearchEngine>))
+            {
+                var service = fixture.Freeze<Mock<ISearchEngineService>>();
+                Assert.Throws<IntegrationFixtureException>(() => fixture.Freeze<Mock<ISearchEngineService>>());
+            }
+        }
+
+        [Fact]
+        public void DoubleCallFixture()
+        {
+            using (var fixture = new Fixture<Startup>())
+            {
+                var service = fixture.Freeze<Mock<ISearchEngineService>>();
+                Assert.Throws<IntegrationFixtureException>(() => fixture.Freeze<Mock<ISearchEngineService>>());
+            }
+        }
+
+        [Fact]
+        public void WrongClassFixture()
+        {
+            using (var fixture = new Fixture<Startup>())
+            {
+                Assert.Throws<IntegrationFixtureException>(() => fixture.Freeze<FreezeTests>());
+            }
+        }
+
+        [Fact]
+        public void WrongClassRefitFixture()
+        {
+            using (var fixture = new RefitFixture<Startup, ISearchEngine>(RestService.For<ISearchEngine>))
+            {
+                Assert.Throws<IntegrationFixtureException>(() => fixture.Freeze<FreezeTests>());
+            }
+        }
+    }
+}
