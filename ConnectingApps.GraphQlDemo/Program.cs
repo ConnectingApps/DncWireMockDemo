d771b0e07f1eafc90424960f11f4ccd8d323f3ca
Daan Acohen <daan.acohen@connectingapps.net>
2020-06-06 22:25:42 +0200

    Added GraphQl project

---
+++ ConnectingApps.GraphQlDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConnectingApps.GraphQlDemo
+{
+    public class Program
+    {
+        public static Task Main(string[] args) =>
+            CreateWebHostBuilder(args).Build().RunAsync();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
