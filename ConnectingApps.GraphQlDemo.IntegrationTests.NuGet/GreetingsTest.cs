4306d5de3305ba3407ef1ea5c2c820650ac39f77
Daan Acohen <daan.acohen@connectingapps.net>
2020-06-07 08:50:23 +0200

    Using a NuGet package for GraphQl usage

---
+++ ConnectingApps.GraphQlDemo.IntegrationTests.NuGet/GreetingsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using ConnectingApps.IntegrationFixture.Shared.GraphQl;
+using FluentAssertions.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace ConnectingApps.GraphQlDemo.IntegrationTests.NuGet
+{
+    public class GreetingsTest
+    {
+        [Fact]
+        public async Task HelloTest()
+        {
+            // arrange
+            using (var graphQlFixture = new GraphQlFixture<Startup>("playground/.."))
+            {
+                var client = graphQlFixture.GetClient();
+
+                // act
+                var response = await client.ExecuteQuery("{ greetings { hello }}");
+
+                // assert
+                string expectedJson = "{\"data\":{\"greetings\":{\"hello\":\"World\"}}}";
+
+                var actual = JToken.Parse(response.ResponseContent);
+                var expected = JToken.Parse(expectedJson);
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                actual.Should().BeEquivalentTo(expected);
+            }
+        }
+    }
+}
