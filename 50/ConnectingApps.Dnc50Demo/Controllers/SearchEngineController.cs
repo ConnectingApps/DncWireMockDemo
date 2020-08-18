9ce81e514e50aa49126e7ee7133fefc3897e6c64
Daan Acohen <daan.acohen@connectingapps.net>
2020-08-18 15:07:49 +0200

    Added a .NET 5 demo project

---
+++ 50/ConnectingApps.Dnc50Demo/Controllers/SearchEngineController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using ConnectingApps.Dnc50Demo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectingApps.Dnc50Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchEngineController : ControllerBase
+    {
+        private readonly ISearchEngineService _searchEngineService;
+
+        public SearchEngineController(ISearchEngineService searchEngineService)
+        {
+            _searchEngineService = searchEngineService;
+        }
+
+        [HttpGet("{queryEntry}", Name = "GetNumberOfCharacters")] // http://localhost:61712/api/searchengine/daan
+        public async Task<ActionResult<int>> GetNumberOfCharacters(string queryEntry)
+        {
+            var numberOfCharacters = await _searchEngineService.GetNumberOfCharactersFromSearchQuery(queryEntry);
+            return Ok(numberOfCharacters);
+        }
+    }
+}
---
+++ 50/ConnectingApps.Dnc50Demo/Services/SearchEngineService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConnectingApps.Dnc50Demo.Services
+{
+    public class SearchEngineService : ISearchEngineService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SearchEngineService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<int> GetNumberOfCharactersFromSearchQuery(string toSearchFor)
+        {
+            var result = await _httpClient.GetAsync($"/search?q={toSearchFor}");
+            var content = await result.Content.ReadAsStringAsync();
+            return content.Length;
+        }
+    }
+}
