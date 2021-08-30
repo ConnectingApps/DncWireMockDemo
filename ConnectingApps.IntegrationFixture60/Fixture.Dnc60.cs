5aa63f10472e3d48e5544bf20fc06e472f045e61
Daan Acohen <daan.acohen@connectingapps.net>
2021-08-30 16:44:39 -0700

    Create integrationfixture 6.0

---
+++ ConnectingApps.IntegrationFixture60/Fixture.Dnc60.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConnectingApps.IntegrationFixture
+{
+    public partial class Fixture<TStartup> : IDisposable, IAsyncDisposable where TStartup : class
+    {
+        public async ValueTask DisposeAsync()
+        {
+            _factory.Dispose();
+            if (_serviceScope.IsValueCreated)
+            {
+                await _serviceProvider.DisposeAsync();
+                _serviceScope.Value.Dispose();
+            }
+        }
+    }
+}
